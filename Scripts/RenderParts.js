400e8941c548fec931bafbc66e1daf1dc93d8d1e
Joel Austin <Jta26@pitt.edu>
2017-04-18 15:39:09 -0400

    Initial commit
    Uploaded all the initial files for the website.

---
+++ Scripts/RenderParts.js
@@ -0,0 +1,18 @@
+﻿$(function() {
+    $("#footerDiv").load("footer.html");
+});
+$(function () {
+    $("#navDiv").load("nav.html");
+});
+$(function () {
+    $("#alternateNavDiv").load("alternateNav.html");
+});
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-92507606-1', 'auto');
+        ga('send', 'pageview');
