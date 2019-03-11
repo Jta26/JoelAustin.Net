f0a674287279db27e679ad2a11ff36fe8cac87d0
Jta26 <Jta26@Pitt.edu>
2019-03-11 02:36:01 -0400

    Blog Window Maintains Content on Refresh.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -10,12 +10,48 @@ class Blog extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentImage: 0
+            currentImage: 0,
+            title: '',
+            date: '',
+            paragraphs: ['',''],
+            images: ['']
         }
+        this.getInitialState = this.getInitialState.bind(this);
+        this.storeData = this.storeData.bind(this);
         this.openLightbox = this.openLightbox.bind(this);
         this.closeLightbox = this.closeLightbox.bind(this);
         this.goToNext = this.goToNext.bind(this);
         this.goToPrevious = this.goToPrevious.bind(this);
+    }
+    getInitialState() {
+        
+        var title =  this.props.location.state.title || localStorage.getItem('title');
+        var date = this.props.location.state.date || localStorage.getItem('date');
+        var paragraphs = this.props.location.state.paragraphs || localStorage.getItem('paragraphs');
+        var images = this.props.location.state.images || localStorage.getItem('images');
+        console.log('INDATA:', title, date, paragraphs, images);
+        this.setState({
+            title: title,
+            date: date,
+            paragraphs: Array.from(paragraphs),
+            images: Array.from(images)
+        });
+        
+        
+    }
+    storeData() {
+        window.onbeforeunload = () => {
+            console.log('Storing STATE');
+            localStorage.setItem('title', this.state.title);
+            localStorage.setItem('date', this.state.date);
+            localStorage.setItem('paragraphs', this.state.paragraphs);
+            localStorage.setItem('images', this.state.images);
+        }
+    }
+    componentDidMount() {
+        this.storeData()
+        this.getInitialState();
+
     }
     openLightbox(index) {
         this.setState({
@@ -48,23 +84,24 @@ class Blog extends Component {
                     <div className='blog-header'>
                         <div className='blog-header-bkrnd'></div>
                         <div className='blog-header-content'>
-                            <h1>{this.props.location.state.title}</h1>
-                            <h2>{this.props.location.state.date}</h2>
+                            <h1>{this.state.title}</h1>
+                            <h2>{this.state.date}</h2>
                         </div>
                     </div>
                     <div className='blog-content'>
                     {
-                        this.props.location.state.paragraphs.map(paragraph => {
+                        this.state.paragraphs.map(paragraph => {
                         
                             return <p className='paragraph'>{parse(paragraph)}</p>
                         })
                     }
                      <div className='blog-content-imgs'>
-                        {this.props.location.state.images.map((img, i) => {
+                        {this.state.images.map((img, i) => {
                             return <img src={img.src} onClick={this.openLightbox.bind(this, i)}/>                     
-                            })}
+                            })
+                        }
                      </div>
-                     <LightBox images={this.props.location.state.images}
+                     <LightBox images={this.state.images}
                             onClose={this.closeLightbox}
                             onClickPrev={this.goToPrevious}
                             onClickNext={this.goToNext}
